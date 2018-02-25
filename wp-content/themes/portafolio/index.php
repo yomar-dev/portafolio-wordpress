9e8acd70bb6491065bc27a14a462b9e1a3e655c7
Yomar Miranda <mirandayomar@gmail.com>
2018-02-25 18:20:50 -0500

    Obteniendo información de nuestro blog con la función 'bloginfo()'.

--- wp-content/themes/portafolio/index.php
+++ wp-content/themes/portafolio/index.php
@@ -3,10 +3,18 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Mi Portafolio en Wordpress</title>
+	<title><?= bloginfo('title'); ?></title>
 	<link rel="stylesheet" href="">
 </head>
 <body>
 	<h1><?= bloginfo('name'); ?></h1>
+
+	<ul>
+		<li><strong>Descripción: </strong><?= bloginfo('description'); ?></li>
+		<li><strong>URL: </strong><?= bloginfo('url'); ?></li>
+		<li><strong>Lenguaje: </strong><?= bloginfo('language'); ?></li>
+		<li><strong>URL Hoja de Estilos: </strong><?= bloginfo('stylesheet_url'); ?></li>
+		<li><strong>Directorio Hoja de Estilos: </strong><?= bloginfo('stylesheet_directory'); ?></li>
+	</ul>
 </body>
 </html>
