96b4d7e279fdf8649ec63891514e330b5a16956f
Yomar Miranda <mirandayomar@gmail.com>
2018-02-25 20:36:50 -0500

    Separamos el sidebar en un archivo separado y lo incluimos en el index.

---
+++ wp-content/themes/portafolio/sidebar.php
@@ -0,0 +1,14 @@
+<div class="block grid--item-3">
+	<div class="block__title">
+		Sidebar
+	</div>
+	<div class="block__body">
+		<ul>
+			<li>Virtuously</li>
+			<li>Grifter</li>
+			<li>Rentaller</li>
+			<li>Unde</li>
+			<li>Bitty</li>
+		</ul>
+	</div>
+</div>
