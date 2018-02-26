a140bc18c0460409185f5ac1554f490820997a17
Yomar Miranda <mirandayomar@gmail.com>
2018-02-25 19:19:45 -0500

    Modelo de plantilla básica para nuestro portafolio.

--- wp-content/themes/portafolio/index.php
+++ wp-content/themes/portafolio/index.php
@@ -7,6 +7,34 @@
 	<link rel="stylesheet" href="<?= bloginfo('stylesheet_url'); ?>">
 </head>
 <body>
-	<h1><?= bloginfo('name'); ?></h1>
+	<h1 class="page-heading max-width"><?= bloginfo('name'); ?></h1>
+
+	<div class="grid max-width">
+		<div class="block grid--item-9">
+			<div class="block__title">
+				Bloque Principal
+			</div>
+			<div class="block__body">
+				<p>
+					Cottonseed embryoniferous dialogite osteohalisteresis eudaemonics ultimateness buckboard untheologize fumet subbeau admaxillary headache Notharctidae ayacahuite resina impresa achingly unexperiencedness accurately pilikai unidentate onychotrophy bastide unimpedness.
+				</p>
+			</div>
+		</div>
+
+		<div class="block grid--item-3">
+			<div class="block__title">
+				Sidebar
+			</div>
+			<div class="block__body">
+				<ul>
+					<li>Virtuously</li>
+					<li>Grifter</li>
+					<li>Rentaller</li>
+					<li>Unde</li>
+					<li>Bitty</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
