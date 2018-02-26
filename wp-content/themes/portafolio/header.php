2060f2e05b54dc4786d7d3ad671de2f59ad18104
Yomar Miranda <mirandayomar@gmail.com>
2018-02-25 19:34:43 -0500

    Separamos el header de nuestro tema en un archivo aparte para luego incluirlo donde lo necesitemos.

---
+++ wp-content/themes/portafolio/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title><?= bloginfo('title'); ?></title>
+	<link rel="stylesheet" href="<?= bloginfo('stylesheet_url'); ?>">
+</head>
--- wp-content/themes/portafolio/index.php
+++ wp-content/themes/portafolio/index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title><?= bloginfo('title'); ?></title>
-	<link rel="stylesheet" href="<?= bloginfo('stylesheet_url'); ?>">
-</head>
 <body>
 	<h1 class="page-heading max-width"><?= bloginfo('name'); ?></h1>
 
