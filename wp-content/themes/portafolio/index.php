6e42f3f23983bc9c6a2300d3c8f363842fd67bd2
Yomar Miranda <mirandayomar@gmail.com>
2018-02-25 19:06:50 -0500

    Incluir hoja de estilos utilizando 'bloginfo()'.

--- wp-content/themes/portafolio/index.php
+++ wp-content/themes/portafolio/index.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title><?= bloginfo('title'); ?></title>
-	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="<?= bloginfo('stylesheet_url'); ?>">
 </head>
 <body>
 	<h1><?= bloginfo('name'); ?></h1>
