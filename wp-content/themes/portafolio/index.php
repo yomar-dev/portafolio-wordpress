4925a60ff8d2eb41f8b2b5c7a68309d1d283aab4
Yomar Miranda <mirandayomar@gmail.com>
2018-02-25 19:10:42 -0500

    Eliminamos el contenido que no necesitamos.

--- wp-content/themes/portafolio/index.php
+++ wp-content/themes/portafolio/index.php
@@ -8,13 +8,5 @@
 </head>
 <body>
 	<h1><?= bloginfo('name'); ?></h1>
-
-	<ul>
-		<li><strong>Descripción: </strong><?= bloginfo('description'); ?></li>
-		<li><strong>URL: </strong><?= bloginfo('url'); ?></li>
-		<li><strong>Lenguaje: </strong><?= bloginfo('language'); ?></li>
-		<li><strong>URL Hoja de Estilos: </strong><?= bloginfo('stylesheet_url'); ?></li>
-		<li><strong>Directorio Hoja de Estilos: </strong><?= bloginfo('stylesheet_directory'); ?></li>
-	</ul>
 </body>
 </html>
