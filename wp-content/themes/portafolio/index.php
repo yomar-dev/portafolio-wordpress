70f93a223e1db5f1860aa21da49946f8d42e8ca9
Yomar Miranda <mirandayomar@gmail.com>
2018-02-25 22:42:14 -0500

    Ahora encerramos todas nuestras entradas dentro de etiquetas 'articles'.

--- wp-content/themes/portafolio/index.php
+++ wp-content/themes/portafolio/index.php
@@ -11,10 +11,12 @@
 				<?php if(have_posts()): ?>
 					<?php while (have_posts()): ?>
 						<?php the_post(); ?>
-						<h2><?php the_title(); ?></h2>
 
-						<?php the_excerpt(); ?>
-						
+						<article>							
+							<h2><?php the_title(); ?></h2>
+							<?php the_excerpt(); ?>
+						</article>
+
 					<?php endwhile; ?>
 				<?php endif; ?>
 			</div>
