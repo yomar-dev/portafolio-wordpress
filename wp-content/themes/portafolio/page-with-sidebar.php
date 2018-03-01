d9c5a800dbf84317e975fd2e9ef949a17ce9d3e0
Yomar Miranda <mirandayomar@gmail.com>
2018-02-28 22:20:07 -0500

    Creamos una plantilla que puede ser usada al momento de crear una página.

---
+++ wp-content/themes/portafolio/page-with-sidebar.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * Template Name: Página con Sidebar
+ */
+?>
+<?php get_header(); ?>
+<body>
+				<?php if(have_posts()): ?>
+					<?php while (have_posts()): ?>
+						<?php the_post(); ?>
+						<h1 class="page-heading max-width"><span>Titulo: </span><?php the_title(); ?></h1>
+
+						<div class="grid max-width">
+							<div class="block grid--item-9">
+								<div class="block__body">
+									<?php the_content(); ?>
+								</div>
+							</div>
+							<?php get_sidebar(); ?>
+						</div>
+					<?php endwhile; ?>
+				<?php else: ?>
+					<h4>No se encontraron entradas</h4>
+				<?php endif; ?>
+	<?php get_footer(); ?>
