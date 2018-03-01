c7a254d338af6c85f71e0e2a03a7eaf962791238
Yomar Miranda <mirandayomar@gmail.com>
2018-02-28 21:21:51 -0500

    Footer para las entradas en la vista de single.

---
+++ wp-content/themes/portafolio/footer-single.php
@@ -0,0 +1,16 @@
+	<footer class="max-width">
+		<div class="block max-width">
+				<div class="block__title">
+					Footer Single
+				</div>
+				<div class="block__body">
+					<p>
+						Entrada del portafolio
+					</p>
+				</div>
+			</div>
+	</footer>
+	<?php wp_footer(); ?>
+
+</body>
+</html>
